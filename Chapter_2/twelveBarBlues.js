c9f66e6f4c4b1224dcb2f9042f20644dcf5e224d
Greg Weber <gregoryweberguitars@gmail.com>
2017-12-27 20:53:23 -0800

    fun function using console.log and a for loop to print out a 12 bar blues pattern

---
+++ Chapter_2/twelveBarBlues.js
@@ -0,0 +1,11 @@
+//write a function that prints the number 1, then 'chink'
+//then 'boom', then 'chick' then 2 the 'chick', 'boom', //'chick'- counting the sme for each cycle upto and //including 12.
+
+
+function theBlues(){
+    for (var i = 1; i <= 12; i++){
+        console.log(`${i} chick boom chick`);
+    }
+}
+
+theBlues();
