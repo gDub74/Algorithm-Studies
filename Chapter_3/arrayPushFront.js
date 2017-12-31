a866548ed86b726f26dffb2753c8eb5874bac5f5
Greg Weber <gregoryweberguitars@gmail.com>
2017-12-31 14:29:50 -0800

    function that takes in an array and a value and returns the array with the value appended to the first position of the array and everything else shifted to the the right by one index therby ending with an array length one longr than start.

---
+++ Chapter_3/arrayPushFront.js
@@ -0,0 +1,15 @@
+//Given an array, insest an additional value at the begining of the array.
+
+
+function pushFront(arr, value){
+    var lastIdx = arr[arr.length-1];
+    for (var i=(arr.length-1); i>0; i--){
+        arr[i]= arr[i-1];
+    }
+    arr.push(lastIdx);
+    arr[0]= value;
+    return arr;
+}
+
+console.log(pushFront([1,2,3,4], 0));
+console.log(pushFront([2,21,'hey!',94], 'push it up front!'));
