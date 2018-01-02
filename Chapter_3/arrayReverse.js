976a3f3906eecf0dcff85f326252663bd191d991
Greg Weber <gregoryweberguitars@gmail.com>
2018-01-02 11:48:34 -0800

    function returns an array in reverse order from given array

---
+++ Chapter_3/arrayReverse.js
@@ -0,0 +1,19 @@
+//reverse order of array without using a new array to store values
+
+
+//plan:
+//swap fisrt and last, then second and second to last ...
+
+
+function arrayReverser(arr){
+    //loop through to the middle of the array and swap curent index with arr length minus the index
+    for (var i = 0; i < arr.length/2; i++){
+        var temp = arr[i];
+        arr[i] = arr[arr.length-1-i]; 
+        arr[arr.length-1-i] = temp;
+    }
+    return arr;
+}
+
+console.log(arrayReverser([1,2,3,4,5,6,7]));
+console.log(arrayReverser([1,2,3,4,5,6,7,8]));
