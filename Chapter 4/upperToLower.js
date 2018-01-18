7ce2f246dca9111f914e763c7efc4f31e1966090
Greg Weber <gregoryweberguitars@gmail.com>
2018-01-18 11:23:18 -0800

    testing one case of chenging upper to lowercase, need to elaborate so that it workes for whole alphabet

---
+++ Chapter 4/upperToLower.js
@@ -0,0 +1,17 @@
+//chenge uppercase to lowercase in a string
+
+
+x = 'A'
+
+function upperToLower(str){
+    console.log(str);
+    var upper = [];
+    var checker = {'A':'a'};
+   if (str == "A"){
+    str = checker['A'];
+   }
+   console.log(str);
+}
+
+upperToLower(x);
+
