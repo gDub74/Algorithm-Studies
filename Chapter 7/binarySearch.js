cd1d31dbf5bd7e59af92180bce3446812a13dc73
Greg Weber <gregoryweberguitars@gmail.com>
2018-02-11 18:20:29 -0800

    reworked for clarity

--- Chapter 7/binarySearch.js
+++ Chapter 7/binarySearch.js
@@ -2,22 +2,22 @@
 //taking advantage of the fact that the array is sorted. As always, only use built-in functions that you are prepared to recreate (write yourself) on demand!
 
 var arr1 = [1,2,3,5,6,8,9];
-var val1 = 10;
+var val1 = 4;
 
 function binarySearch(arr, val){
-    // check middle of arr val
-    if (arr.length <= 2){
-        if (arr[0] == val || arr[1] == val){
+    
+    var checkerVal = arr[Math.floor(arr.length/2)];
+    if (checkerVal == val){
+        return true;
+    } 
+    if (arr.length <= 2){ //if we got here thaen we have already checked arr[1] in the last step
+        if (arr[0] == val){
             return true;
         }else{
             return false;
         }
     }
-    var checkerVal = arr[Math.floor(arr.length/2)];
-    if (checkerVal == val){
-        return true;
-    }
-    //else cut the array down to size by half that possible could contain
+    //else cut the array down to size to half that possible could contain search val
     if (checkerVal > val){
         arr.length -= Math.floor(arr.length/2);
         return (binarySearch(arr, val));
@@ -27,9 +27,6 @@ function binarySearch(arr, val){
         var smallerArr = arr.slice(Math.floor(arr.length/2));
         return binarySearch(smallerArr,val);
     } 
-    {
-        return false;
-    }
 }
 
 console.log(binarySearch(arr1,val1));
