c0bfdf3ec2264133b002f4f76cb32c5106c7297b
Greg Weber <gregoryweberguitars@gmail.com>
2018-03-08 20:37:28 -0800

    facrtiorial two ways

---
+++ Chapter 9/r factorial.js
@@ -0,0 +1,41 @@
+// Recursive Factorial
+// Given num, return the product of ints from 1 up to num. If less than zero, treat as zero. If not integer, truncate. Experts tell us 0! is 1. rFact(3) = 6 (1*2*3). Also, rFact(6) = 720 (1*2*3*4*5*6).
+
+
+
+function factorial(num){
+    var product = 1;
+    if (num <= 0){
+        return product;
+    }
+    else{
+        while (num > 0){
+            product *= num;
+            num --;   
+        }
+    }
+    return  product;
+}
+
+console.log(factorial(3)); // 6
+console.log(factorial(4)); // 24
+console.log(factorial(6)); // 720 
+
+
+function rFactorial(num){
+    product = 1;
+    function recursiveFactorial(num){
+        while (num > 1){
+            product *= num;
+            return recursiveFactorial(num-1);
+        }
+        return product
+    }
+    // call the inner function
+    recursiveFactorial(num);
+    return product;
+}
+
+console.log(rFactorial(3)); // 6
+console.log(rFactorial(4)); // 24
+console.log(rFactorial(6)); // 720
