5091e743a2a7b82c50bb5fc8f2a2e92059dea60c
Greg Weber <gregoryweberguitars@gmail.com>
2018-03-13 12:58:26 -0700

    testing out function scoping

--- functionScopeTest.js
+++ functionScopeTest.js
@@ -1,39 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <script>
 
-        //callin returning a whole function, then setting that to a var, then using that var to callthe function
-        function counter(){
-    var count = 0;
+
+
+//returning a child scoped function, then setting that to a var, then using that var to call the parent function and in turn the child functfunction.
+
+function counter(){
+var count = 0;
+//vars declared here are available in the function and in the child finction, but not globally our of the parent function.
 
     function childScope(){
+        // vars declared here are local to this scope
         var childContent = 'child';
         console.log('counting');
         return ++count;         // ++ before the var does operation before it gets returned.
     }
 
     return childScope;
-    }
+}
 
-    x = counter();
-    console.log(x());
-    console.log(x());
-    console.log(x());
-    console.log(x());
-    console.log(x());
-    console.log(x());
-    console.log(x());
-    console.log(x());
-    console.log(x());
-    </script>
-</body>
-</html>
-
+x = counter();
+console.log(x());
+console.log(x());
+console.log(x());
+console.log(x());
+console.log(x());
+console.log(x());
+console.log(x());
+console.log(x());
+console.log(x());
 
