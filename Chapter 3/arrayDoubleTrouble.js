de5243d138b62f3a9150bb36e32bf768e5ad9e5f
Greg Weber <gregoryweberguitars@gmail.com>
2018-03-16 01:46:02 -0700

    recursive function returning squares of numbers

--- Chapter 3/arrayDoubleTrouble.js
+++ Chapter 3/arrayDoubleTrouble.js
@@ -5,12 +5,14 @@
 
 function elementDoubler(arr){
     arr.length += arr.length;  //double the length of the array to start
-
+    console.log(arr);
     //loop thru array and shift values to right, everytime thru outter loop add 2 to index so that you skip the value you just overwrote
     for (var i = 0; i < arr.length; i +=2){
         for (var j = arr.length-1; j>i; j --){
            arr[j]= arr[j-1];
+           console.log(arr);
         }
+        console.log('outer loop')
     }
     return arr;
 }
---
+++ Chapter 9/risingSquares.js
@@ -0,0 +1,18 @@
+// Ever since her dad discovered universal truths about triangles, Sophia Pythagoras has loved square numbers. Given positive integer, successively print squares of integers up to that integer, first ascending odds, then descending evens. Solve recursively with no loops. Ex.: risingSquare(5) returns "1, 9, 25, 16, 4", risingSquare(8) returns "1, 9, 25, 49, 64, 36, 16, 4".
+
+
+
+function recurSquares(num, oddSqrs = '', evenSqrs = ''){
+    if (num >= 1){
+        if (num*num % 2 == 1){
+            oddSqrs = (num*num).toString() + ', ' + oddSqrs;
+            return recurSquares(num -1, oddSqrs, evenSqrs);
+        }
+        evenSqrs += (num*num).toString() + ', ';
+        return recurSquares(num -1, oddSqrs, evenSqrs);    
+    }
+    return oddSqrs + evenSqrs.slice(0, -2);
+}
+
+
+console.log(recurSquares(8));
