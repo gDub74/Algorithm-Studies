5b0ecb2f94180e8d5112882d3cd29b54a63c6956
Greg Weber <gregoryweberguitars@gmail.com>
2018-04-07 16:54:30 -0700

    nothing to show here....

--- Chapter 10/clibingStairs.js
+++ Chapter 10/clibingStairs.js
@@ -4,29 +4,43 @@
 
 
 //return an array of arrays of all the possible combinations of 1's and 2's of a given number, num2's will help us end our recursion
-function binaryConbinations(num, arrBuilderIndex=1, numTwos=Math.floor(num/2), arr=[]) {
-    if (numTwos == 0) { 
-        //maybe add the final array if its a odd starting number 
-        return arr;
-    }
-    //build the 1's
-    if (!arr[0]){
-        arr[0] = [];
-        for (let i = num; i > 0; i--){
-            arr[0].push(1);
-        }
-    }
-    //build 1's and 2's and push
-    let currArr = [];
-    for (let count = 0; count < num; count++){
-        let 
-    }
-    arr.push(currArr);
+// function binaryConbinations(num, arrBuilderIndex=1, numTwos=Math.floor(num/2), arr=[]) {
+//     if (numTwos == 0) { 
+//         //maybe add the final array if its a odd starting number 
+//         return arr;
+//     }
+//     //build the 1's
+//     if (!arr[0]){
+//         arr[0] = [];
+//         for (let i = num; i > 0; i--){
+//             arr[0].push(1);
+//         }
+//     }
+//     //build 1's and 2's and push
+//     let currArr = [];
+//     for (let count = 0; count < num; count++){
+//         let 
+//     }
+//     arr.push(currArr);
 
 
-    // binaryConbinations(num, ++arrBuilderIndex, --numTwos, arr)
-    console.log(arr);
-    console.log(numTwos);
+//     // binaryConbinations(num, ++arrBuilderIndex, --numTwos, arr)
+//     console.log(arr);
+//     console.log(numTwos);
+// }
+
+// binaryConbinations(3);
+
+function climbingStairs(numStairs, totalStairs = numStairs, results=[], currArr=[], currArrSum = 0){
+    if (!numStairs){
+        results.push(currArr)
+        return results;
+    }
+    if (currArrSum < totalStairs){
+        currArr.push(1);
+    }
+    // return climbingStairs(numStairs -1, totalStairs, results, [], 0)
+    return climbingStairs(numStairs-1, totalStairs, results, currArr,  currArrSum+1)
 }
 
-binaryConbinations(3);
+console.log(climbingStairs(4));
